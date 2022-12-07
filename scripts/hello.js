a030a01fae3cb7ec1b3cdd323cd5d6ea50ea8e18
Yassin Rian <yassinrian@hotmail.com>
2022-12-07 16:47:40 +0100

    sixth commit

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,4 +1,4 @@
-requirejs(["https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
+requirejs(["https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
   console.log(say.hello("english"));
   console.log(say.hello("spanish"));
   console.log(say.hello("french"));
