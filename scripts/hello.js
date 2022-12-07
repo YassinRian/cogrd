9fa502143f467d14f26f8005b7760d2769913742
Yassin Rian <yassinrian@hotmail.com>
2022-12-07 12:34:00 +0100

    fourht commit

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,5 +1,13 @@
-requirejs(["https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
+// requirejs(["https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
+//   console.log(say.hello("english"));
+//   console.log(say.hello("spanish"));
+//   console.log(say.hello("french"));
+// });
+
+define(function(require) {
+  "use strict";
+  var say = require("https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js");
   console.log(say.hello("english"));
   console.log(say.hello("spanish"));
   console.log(say.hello("french"));
-});
+})
