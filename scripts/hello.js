9d04661807eebb64c29c54e774f4fbf390b0d63b
Yassin Rian <yassinrian@hotmail.com>
2023-03-17 15:17:49 +0100

    wijziging

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,15 @@
-// requirejs(["https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
+requirejs(["https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
+ return {
+  console.log(say.hello("english"));
+  console.log(say.hello("spanish"));
+  console.log(say.hello("french"));
+ }
+});
+
+// define(function(require) {
+//   "use strict";
+//   var say = require("https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js");
 //   console.log(say.hello("english"));
 //   console.log(say.hello("spanish"));
 //   console.log(say.hello("french"));
 // });
-
-define(function(require) {
-  "use strict";
-  var say = require("https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js");
-  console.log(say.hello("english"));
-  console.log(say.hello("spanish"));
-  console.log(say.hello("french"));
-});
