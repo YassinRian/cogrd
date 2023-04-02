06956dc73b4fe7592f182e8bbbb9a7b85bd9cdc2
YassinRian <yassinrian@hotmail.com>
2023-04-02 18:56:52 +0200

    verwijderd bestanden

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,24 +1,3 @@
-requirejs(["https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js"], function(say) {
- const Contentview = function() {
-  console.log(say.hello("english"));
-  console.log(say.hello("spanish"));
-  console.log(say.hello("french"));
- };
- return Contentview;
-});
-
-// define(function(require) {
-//   "use strict";
-//   var say = require("https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js");
-//   console.log(say.hello("english"));
-//   console.log(say.hello("spanish"));
-//   console.log(say.hello("french"));
-// });
-
-define(function(require) {
-  "use strict";
-  var say = require("https:raw.githubusercontent.com/YassinRian/cogrd/main/scripts/say.js");
-  console.log(say.hello("marocco"));
-  console.log(say.hello("spanish"));
-  console.log(say.hello("french"));
+define(function() {
+oPage.getControlByName( "ValuePrompt1" ).autoSubmit = false;
 });
--- scripts/say.js
+++
@@ -1,7 +0,0 @@
-define(["https://raw.githubusercontent.com/YassinRian/cogrd/main/scripts/translations.js"], function(translations){
-  return {
-    hello: function(language) {
-      return translations[language].hello + " " + translations[language].world;
-    }
-  };
-});
